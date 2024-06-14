62ab965f543e0a00150662e6ae5ecbea024d7ca6
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-06-14 23:07:17 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Home Manager configuration of vasilysterekhov";
+
+  inputs = {
+    # Note: we take nixpkgs from the system flake registry
+    # so that they get auto-updated together
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."vasilysterekhov" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
