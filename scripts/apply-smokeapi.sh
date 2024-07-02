f465fa4ac9fb501697b183b3140c9e146d962f2c
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-07-03 00:33:35 +0100

    Extract most of apply-smokeapi

---
+++ scripts/apply-smokeapi.sh
@@ -0,0 +1,17 @@
+upd_32 () {
+  cp --update=none steam_api.dll steam_api.dll_o
+  cp $smokeapi32_dll steam_api.dll
+}
+upd_64 () {
+  cp --update=none steam_api64.dll steam_api64.dll_o
+  cp $smokeapi64_dll steam_api64.dll
+}
+export -f upd_32
+export -f upd_64
+
+IFS=$'\n'
+for path in $paths; do
+  cd $path
+  find -type f -name "steam_api.dll" -execdir bash -c upd_32 bash \;
+  find -type f -name "steam_api64.dll" -execdir bash -c upd_64 bash \;
+done
