47f4685ba901b26d38d0c65406f81e51f7919022
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-08-06 23:28:03 +0100

    Add hie AST aliases

--- home.nix
+++ home.nix
@@ -181,12 +181,16 @@
     pull-user = "fish -c 'cd ~/.config/home-manager && git fetch && git pull'";
     update-user = "pull-user && rebuild-user switch";
     update-system = "pull-system && rebuild-system switch /etc/nixos";
+    # produce hie AST and nothing more
+    ghc-dump-hie = "ghc -fwrite-ide-info -fno-code -fforce-recomp -ddump-hie";
   };
   programs.fish.functions = {
     # we assume all extra substituters are just for development flakes, and not
     # worth querying for regular system updates
     rebuild-user = "home-manager $argv[1] --option substituters https://cache.nixos.org $argv[2..]";
     rebuild-system = "sudo nixos-rebuild $argv[1] --flake $argv[2]#$argv[3] --option substituters https://cache.nixos.org $argv[4..]";
+
+    ghc-view-hie = "ghc-dump-hie $argv | ${pkgs.bat}/bin/bat --language log";
 
     bash = "begin; set -lx INTERACTIVE_SHELL \"bash\"; command bash $argv; end";
     # Fix unsightly background on vi mode indicator in hydro
