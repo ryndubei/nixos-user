50c0c6b86ecc2916311ced85065e7df8eb0b3815
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-06-01 20:48:11 +0100

    Roll back some keybindings

--- data/pop-shell-keybindings.nix
+++ data/pop-shell-keybindings.nix
@@ -1,19 +1,10 @@
 # https://github.com/NixOS/nixpkgs/issues/314969#issuecomment-2317070849
+# pop-shell keybindings except workspace/monitor controls
 {
   "org/gnome/desktop/wm/keybindings" = {
     close = [ "<Super>q" "<Alt>F4" ];
     minimize = [ "<Super>comma" ];
     toggle-maximized = [ "<Super>m" ];
-    move-to-monitor-left = [ ];
-    move-to-monitor-right = [ ];
-    move-to-monitor-up = [ ];
-    move-to-monitor-down = [ ];
-    move-to-workspace-down = [ ];
-    move-to-workspace-up = [ ];
-    switch-to-workspace-down = [ "<Primary><Super>Down" ];
-    switch-to-workspace-up = [ "<Primary><Super>Up" ];
-    switch-to-workspace-left = [ ];
-    switch-to-workspace-right = [ ];
     maximize = [ ];
     unmaximize = [ ];
   };
@@ -56,12 +47,6 @@
     tile-accept = [ "Return" ];
     tile-reject = [ "Escape" ];
     toggle-stacking-global = [ "<Super>s" ];
-    pop-workspace-down = [ "<Shift><Super>Down" "<Shift><Super>j" ];
-    pop-workspace-up = [ "<Shift><Super>Up" "<Shift><Super>k" ];
-    pop-monitor-left = [ "<Shift><Super>Left" "<Shift><Super>h" ];
-    pop-monitor-right = [ "<Shift><Super>Right" "<Shift><Super>l" ];
-    pop-monitor-down = [ ];
-    pop-monitor-up = [ ];
     focus-left = [ "<Super>Left" "<Super>h" ];
     focus-down = [ "<Super>Down" "<Super>j" ];
     focus-up = [ "<Super>Up" "<Super>k" ];
