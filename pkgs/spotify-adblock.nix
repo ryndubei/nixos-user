7d82faadccfc20dc8b9c66aed204df2e47f9c6bc
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-08-31 18:59:45 +0100

    Fix spotify-adblock hash

--- pkgs/spotify-adblock.nix
+++ pkgs/spotify-adblock.nix
@@ -6,7 +6,7 @@ rustPlatform.buildRustPackage rec {
     owner = "abba23";
     repo = "spotify-adblock";
     rev = "8e0312d6085a6e4f9afeb7c2457517a75e8b8f9d";
-    hash = "sha256-UzpHAHpQx2MlmBNKm2turjeVmgp5zXKWm3nZbEo0mYE=";
+    hash = "sha256-nwiX2wCZBKRTNPhmrurWQWISQdxgomdNwcIKG2kSQsE=";
   };
   cargoLock.lockFile = "${src}/Cargo.lock";
   allowSubstitutes = false;
