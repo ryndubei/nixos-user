ef8d97e33a28be6a395d015ea9c234b6cec43fec
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-09-25 16:04:32 +0100

    Amend librewolf settings

--- desktop.nix
+++ desktop.nix
@@ -148,15 +148,6 @@
   programs.librewolf = {
     enable = true;
     settings = {
-      "browser.safebrowsing.malware.enabled" = true;
-      "browser.safebrowsing.phishing.enabled" = true;
-      "browser.safebrowsing.blockedURIs.enabled" = true;
-      "browser.safebrowsing.provider.google4.gethashURL" =
-        "https://safebrowsing.googleapis.com/v4/threatListUpdates:fetch?$ct=application/x-protobuf&key=%GOOGLE_SAFEBROWSING_API_KEY%&$httpMethod=POST";
-      "browser.safebrowsing.provider.google.gethashURL" =
-        "https://safebrowsing.google.com/safebrowsing/gethash?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2";
-      "browser.safebrowsing.provider.google.updateURL" =
-        "https://safebrowsing.google.com/safebrowsing/downloads?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2&key=%GOOGLE_SAFEBROWSING_API_KEY%";
       "privacy.resistFingerprinting.letterboxing" = true;
       "network.http.referer.XOriginPolicy" = 2;
       "identity.fxaccounts.enabled" = true;
