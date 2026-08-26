b0a235c888cdf9b156fae22201e716d45fbb3db8
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-08-26 15:23:11 +0100

    Remove element, telegram from autostart
    These have settings option to always start minimised, so they will flood
    the entire screen on login if launching via their included desktop
    files.
    
    TODO write custom desktop files later

--- desktop.nix
+++ desktop.nix
@@ -132,9 +132,7 @@
   xdg.autostart.readOnly = true;
   xdg.autostart.entries = [
     "${pkgs.signal-desktop}/share/applications/signal.desktop"
-    "${pkgs.element-desktop}/share/applications/element-desktop.desktop"
     "${pkgs.legcord}/share/applications/legcord.desktop"
-    "${pkgs.telegram-desktop}/share/applications/org.telegram.desktop.desktop"
   ];
 
   gtk.theme = {
