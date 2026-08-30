5a0b5a3c6daafffb8985b7dbc3ddcdbc1e7f132f
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-08-30 03:19:05 +0100

    Add info

--- emacs.nix
+++ emacs.nix
@@ -1,6 +1,9 @@
 {
   programs.emacs.enable = true;
 
+  # Fixes C-h i info index
+  programs.info.enable = true;
+
   services.emacs = {
     enable = true;
     client.enable = true; # Generate desktop file for client
