d46a378375ee19ba1ad5b177bdf1d9c27bbb475d
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-08-31 00:04:11 +0100

    Revert "Use org fork for async previews"
    This reverts commit 7671f7d21bfb40d58c97fbb06608c9cad8e849b9.

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -51,31 +51,3 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
-
-
-; org-mode fork with realtime org latex previews
-; https://abode.karthinks.com/org-latex-preview/#org08777b5
-(package! org :recipe
-  (:host nil :repo "https://git.tecosaur.net/mirrors/org-mode.git" :remote "mirror" :fork
-         (:host nil :repo "https://git.tecosaur.net/tec/org-mode.git" :branch "dev" :remote "tecosaur")
-         :files
-         (:defaults "etc")
-         :build t :pre-build
-         (with-temp-file "org-version.el"
-           (require 'lisp-mnt)
-           (let
-               ((version
-                 (with-temp-buffer
-                   (insert-file-contents "lisp/org.el")
-                   (lm-header "version")))
-                (git-version
-                 (string-trim
-                  (with-temp-buffer
-                    (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
-                    (buffer-string)))))
-             (insert
-              (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
-              (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
-              "(provide 'org-version)\n"))))
-  :pin "1ef59f0aa02e3cff40bae68b756a29bc2001739e")
-
