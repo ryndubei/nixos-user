7f4a1e21cf69c7e0b2bc83eb4a7c2a270f5b48a0
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-09-01 22:12:43 +0100

    Expand using a single click in Treemacs

--- doom.d/config.el
+++ doom.d/config.el
@@ -63,3 +63,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+
+;; Expand using a single click in Treemacs instead of a double click
+(with-eval-after-load 'treemacs
+  (define-key treemacs-mode-map [mouse-1] #'treemacs-single-click-expand-action))
